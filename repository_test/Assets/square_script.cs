e58d469e5d5541fe1cb9c8194597fc1e3681eeb5
Frizlix <38812955+Frizlix@users.noreply.github.com>
2018-04-28 17:08:19 +0200

    rosa melano zorrilla commit
    https://www.youtube.com/watch?v=TFS316SzGFQ

--- repository_test/Assets/square_script.cs
+++ repository_test/Assets/square_script.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class square_script : MonoBehaviour {
-
+    float speed = 10;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +11,6 @@ public class square_script : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate(10*Time.deltaTime,10*Time.deltaTime,10*Time.deltaTime);
+		transform.Rotate(1 * speed * Time.deltaTime, 1*speed*Time.deltaTime, 1 * speed * Time.deltaTime);
 	}
 }
